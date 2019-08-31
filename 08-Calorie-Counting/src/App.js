d6dd5f450bfa006dc256e9bd507150984ec6a28b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-09-01 00:13:46 +0700

    Setup files

---
+++ 08-Calorie-Counting/src/App.js
@@ -0,0 +1,26 @@
+import {
+    h,
+    diff,
+    patch
+} from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
+
+// impure code below
+function app(initModel, update, view, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
+
+    function dispatch(msg) {
+        model = update(msg, model);
+        const updatedView = view(dispatch, model);
+        const patches = diff(currentView, updatedView);
+        rootNode = patch(rootNode, patches);
+        //node.replaceChild(updatedView, currentView);
+        currentView = updatedView;
+    }
+
+}
+
+export default app;
