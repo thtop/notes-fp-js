5ff6c88b79141766b2a2fc9257a885b63bd8fc7f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-31 22:18:28 +0700

    App Function

--- 07-Simple-Counter/counter/src/index.js
+++ 07-Simple-Counter/counter/src/index.js
@@ -8,19 +8,19 @@ const {
 
 const initModel = 0;
 
-function view(model) {
+function view(dispatch, model) {
     return div([
         div({
             className: 'mv2'
         }, `Count:  ${model}`),
         button({
                 className: 'pv1 ph2 mr2',
-                onclick: () => console.log('+ clicked!')
+                onclick: () => dispatch('plus')
             },
             '+'),
         button({
             className: 'pv1 ph2',
-            onclick: () => console.log('- clicked!')
+            onclick: () => dispatch('minus')
         }, '-')
     ])
 }
@@ -36,7 +36,24 @@ function update(msg, model) {
     }
 }
 
+// impure code below
+function app(initModel, update, view, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    node.appendChild(currentView);
+
+    function dispatch(msg) {
+        model = update(msg, model);
+        const updatedView = view(dispatch, model);
+        node.replaceChild(updatedView, currentView);
+        currentView = updatedView;
+    }
+
+}
+
 
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(update('minus', initModel)));
+app(initModel, update, view, rootNode);
+
+//rootNode.appendChild(view(update('minus', initModel)));
